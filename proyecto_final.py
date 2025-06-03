015320ff104f6557288455f4cd15b6d79ed410ca
paisbrayan <brayanpaisportillo@gmail.com>
2025-06-02 19:55:34 -0600

    Add files via upload

---
+++ proyecto_final.py
@@ -0,0 +1,11 @@
+rive_pers={"Nombre":"Rivelino",
+ "Apellidos":"Reyes Gonzales",
+ "Edad":30,
+ "N°.trabajador":123456,
+ "Sexo":"Hombre",
+}
+rive_asis={"Dias laborales":"L,M,V",
+           "Horario":"7:00-14:30",
+           "Retardos menores":0,
+           "Retardos mayores":0,
+           "Faltas":0}
